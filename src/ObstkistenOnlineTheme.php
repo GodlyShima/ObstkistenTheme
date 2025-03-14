71420155477d7a56c7cf7a89624872d0f254c5ab
Godlyshima <lenknutzen@gmx.de>
2025-03-14 12:26:11 +0100

    init

---
+++ src/ObstkistenOnlineTheme.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace ObstkistenOnlineTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class ObstkistenOnlineTheme extends Plugin implements ThemeInterface
+{
+	    public function getThemeConfigPath(): string
+    {
+        return 'theme.json';
+    }
+}
